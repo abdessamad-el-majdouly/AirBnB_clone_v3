b18620d8107f448b4d49df69d0414444b5c622b1
Abdessamad El Majdouly <33199278+abdessamad-el-majdouly@users.noreply.github.com>
2024-01-08 21:46:16 +0100

    Update places_amenities.py

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -21,34 +21,22 @@ def place_amenities(place_id):
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', strict_slashes=False, methods=['DELETE', 'POST'])
 def update_place_amenities(place_id, amenity_id):
     """ Delete, or link/unlink Amenity to/from Place based on the HTTP method """
-    env = getenv('HBNB_TYPE_STORAGE')
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
+    env = getenv('HBNB_TYPE_STORAGE')
+
     if place is None or amenity is None:
         abort(404)
 
     if request.method == 'DELETE':
-        if env == 'db':
-            if amenity in place.amenities:
-                place.amenities.remove(amenity)
-            else:
-                abort(404)
-        else:
-            if amenity.id in place.amenity_ids:
-                place.amenity_ids.remove(amenity.id)
-            else:
-                abort(404)
-        place.save()
-        return make_response(jsonify({}), 200)
+        if amenity not in place.amenities:
+            abort(404)
+        place.amenities.remove(amenity)
 
-    if request.method == 'POST':
-        if env == 'db':
-            if amenity in place.amenities:
-                return make_response(jsonify(amenity.to_dict()), 200)
-            place.amenities.append(amenity)
-        else:
-            if amenity.id in place.amenity_ids:
-                return make_response(jsonify(amenity.to_dict()), 200)
-            place.amenity_ids.append(amenity.id)
-        place.save()
-        return make_response(jsonify(amenity.to_dict()), 201)
+    elif request.method == 'POST':
+        if amenity in place.amenities:
+            return make_response(jsonify(amenity.to_dict()), 200)
+        place.amenities.append(amenity)
+
+    place.save()
+    return make_response(jsonify(amenity.to_dict()), 200 if request.method == 'DELETE' else 201)
