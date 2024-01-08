64d73a71ad2ca3b77b302c7fdfe538923c7166c3
Abdessamad El Majdouly <33199278+abdessamad-el-majdouly@users.noreply.github.com>
2024-01-08 21:36:31 +0100

    Update places_amenities.py

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-""" Endpoints for place_amenity related
-    interactions
-"""
+""" Endpoints for place_amenity related interactions """
+
 from api.v1.views import app_views
 from flask import abort, jsonify, request, make_response
 from models import storage
@@ -10,27 +9,18 @@ from models.amenity import Amenity
 from os import getenv
 
 
-@app_views.route('/places/<place_id>/amenities', strict_slashes=False,
-                 methods=['GET'])
+@app_views.route('/places/<place_id>/amenities', strict_slashes=False, methods=['GET'])
 def place_amenities(place_id):
-    """search for a place with given id and:
-       return a list of its amenities
-    """
+    """ Retrieve the list of all Amenity objects of a Place """
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
     return jsonify([amenity.to_dict() for amenity in place.amenities])
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 strict_slashes=False,
-                 methods=['DELETE', 'POST'])
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', strict_slashes=False, methods=['DELETE', 'POST'])
 def update_place_amenities(place_id, amenity_id):
-    """search for a place and amenity with given ids and:
-        1. remove/unlink amenity from the place
-        2. add/link amenity to the place
-       depending on the method
-    """
+    """ Delete, or link/unlink Amenity to/from Place based on the HTTP method """
     env = getenv('HBNB_TYPE_STORAGE')
     place = storage.get(Place, place_id)
     amenity = storage.get(Amenity, amenity_id)
@@ -40,12 +30,12 @@ def update_place_amenities(place_id, amenity_id):
     if request.method == 'DELETE':
         if env == 'db':
             if amenity in place.amenities:
-                place.amenities.pop(place.amenities.index(amenity))
+                place.amenities.remove(amenity)
             else:
                 abort(404)
         else:
             if amenity.id in place.amenity_ids:
-                place.amenity_ids.pop(place.amenity_ids.index(amenity.id))
+                place.amenity_ids.remove(amenity.id)
             else:
                 abort(404)
         place.save()
